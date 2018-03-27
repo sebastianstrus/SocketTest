f16b37cca939cc8cf8ddb38c6519932dead98052
Sebastian Strus <sebastian.strus@opensolution.se>
2018-03-27 11:17:17 +0200

    First commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SocketTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SocketTest
+  pod 'SwiftSocket'
+end
--- SocketTest/ViewController.swift
+++ SocketTest/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import SwiftSocket
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        let client = TCPClient(address: "192.168.20.111", port: 5188)
+
+        switch client.connect(timeout: 6) {
+        case .success:
+            //sleep(1)
+            switch client.send(string: "47\n{\"version\":\"1\",\"message_type\":\"get_properties\"}" ) {
+            case .success:
+                sleep(1)
+                guard let data = client.read(1024*10) else { return }
+                
+                if let response = String(bytes: data, encoding: .utf8) {
+                    print("response: \(response)")
+                }
+            case .failure(let error):
+                print("1: \(error)")
+            }
+        case .failure(let error):
+            print("2: \(error)")
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
